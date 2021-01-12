3a244a2203d2340c6d8aa06e48d97a662106b3c3
JingBh <jingbohao@yeah.net>
2021-01-12 20:52:11 +0800

    Update app name.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'ScoreDB Server'),
+    'name' => env('APP_NAME', 'ScoreDB v3 API Server'),
 
     /*
     |--------------------------------------------------------------------------
